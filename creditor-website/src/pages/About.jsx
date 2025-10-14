2eaaf266b8258b0c04054001e00b9aa393135dbc
Aiden-Williams-19 <aidenwilliams336@gmail.com>
2025-10-14 14:43:56 +0200

    about page added

---
+++ creditor-website/src/pages/About.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function About() {
+  return (
+    <div className="about">
+      <h1>About Us</h1>
+      <p>Creditor is dedicated to helping clients manage debt responsibly. Our mission is to provide reliable, professional, and effective debt solutions for individuals and businesses alike.</p>
+    </div>
+  );
+}
+
+export default About;
