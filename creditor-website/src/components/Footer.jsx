06b56163871e89e36934febba168a23c01562dc0
Aiden-Williams-19 <aidenwilliams336@gmail.com>
2025-10-14 14:40:24 +0200

    Footer added

---
+++ creditor-website/src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer>
+      <p>© 2025 Creditor. All Rights Reserved.</p>
+      <p>Regulated by relevant authorities.</p>
+    </footer>
+  );
+}
+
+export default Footer;
