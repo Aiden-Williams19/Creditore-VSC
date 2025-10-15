f0347c2f7400713263241ced2d548c326806c014
Aiden-Williams-19 <aidenwilliams336@gmail.com>
2025-10-15 09:58:46 +0200

    split up css

--- creditor-website/src/main.jsx
+++ creditor-website/src/main.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import './styles.css'
+// Centralized stylesheet index that aggregates base, layout, components, pages and responsive modules
+import './styles/index.css'
 
 // main.jsx bootstraps the React application into the #root element created in index.html
 // React.StrictMode helps spot potential problems in development (renders components twice in dev)
