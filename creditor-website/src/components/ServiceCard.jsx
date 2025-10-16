7be3df2202e993b572fc1485747ef0b93ff64909
Aiden-Williams-19 <aidenwilliams336@gmail.com>
2025-10-17 00:46:30 +0200

    learn more bug fixed

--- creditor-website/src/components/ServiceCard.jsx
+++ creditor-website/src/components/ServiceCard.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 // ServiceCard: small reusable card used on the Home page to highlight services.
 // Props:
@@ -15,7 +16,11 @@ function ServiceCard({ title, description, link, image }) {
       <div className="service-card__content">
         <h3>{title}</h3>
         <p>{description}</p>
-        {link && <a href={link} className="btn">Learn More</a>}
+        {link && (link.startsWith('http') ? (
+          <a href={link} className="btn" target="_blank" rel="noreferrer">Learn More</a>
+        ) : (
+          <Link to={link} className="btn">Learn More</Link>
+        ))}
       </div>
     </div>
   );
