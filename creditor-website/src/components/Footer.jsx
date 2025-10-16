874ff98026ef43bf399f29d79df86fbd983ba6c3
Aiden-Williams-19 <aidenwilliams336@gmail.com>
2025-10-16 20:11:26 +0200

    services removed from footer

--- creditor-website/src/components/Footer.jsx
+++ creditor-website/src/components/Footer.jsx
@@ -12,13 +12,7 @@ function Footer() {
           <p>Email: info@creditore.co.za</p>
           <p>We are regulated by the NCR.</p>
         </div>
-        <div className="footer-links">
-          <h4>Services</h4>
-          <ul>
-            <li><a href="/debt-review">Debt Review</a></li>
-            <li><a href="/debt-mediation">Debt Mediation</a></li>
-          </ul>
-        </div>
+        {/* services links removed per request */}
       </div>
     </footer>
   );
